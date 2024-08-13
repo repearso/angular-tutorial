bdc9a17cb2e6323fdef76ffa86ce06f504716426
Rennie Pearson (repearso) <repearso@cisco.com>
2024-08-13 16:13:24 -0400

    Module 2 Complete with search bar and CSS

--- homes-app-start/src/app/app.component.ts
+++ homes-app-start/src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HomeComponent} from './home/home.component';
 
 @Component({
-  standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
+  standalone: true,
+  imports: [HomeComponent],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
