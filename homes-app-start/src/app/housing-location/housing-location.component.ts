61745d4321582beb2d673e122c5fe60b330adf4a
Rennie Pearson (repearso) <repearso@cisco.com>
2024-08-15 13:18:28 -0400

    added link functionality to components nd home

--- homes-app-start/src/app/housing-location/housing-location.component.ts
+++ homes-app-start/src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HousingLocation} from '../housing-location';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
+  imports: [RouterLink, RouterOutlet], 
   template: `
     <section class="listing">
       <img
@@ -16,6 +18,7 @@ import {HousingLocation} from '../housing-location';
       />
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrls: ['./housing-location.component.css']
